91cac2286daf3a42a3a0cef9e9316da0e67a39eb
Josias Iquabius <iquabius@gmail.com>
2017-04-27 11:34:09 -0400

    Add navigation with the Link component

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default () => (
   <div>
@@ -7,6 +8,13 @@ export default () => (
       <meta charSet='utf-8' />
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
     </Head>
+
+    <header>
+      <nav>
+        <Link href='/'><a>Home</a></Link> |
+      </nav>
+    </header>
+
     <p>Olá, esta é a aplicação 'frontend' para o projeto Olimat!</p>
     <hr />
     <p>Olhe a aba do navegador ☝</p>
