458d98736da23de2a0e757c3b9dde45e918dd73f
Josias Iquabius <iquabius@gmail.com>
2017-04-27 11:21:57 -0400

    Add title with the Head component

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,15 @@
+import Head from 'next/head'
+
 export default () => (
-  <div>Olá, esta é a aplicação 'frontend' para o projeto Olimat!</div>
+  <div>
+    <Head>
+      <title>Olimat 🔥</title>
+      <meta charSet='utf-8' />
+      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+    </Head>
+    <p>Olá, esta é a aplicação 'frontend' para o projeto Olimat!</p>
+    <hr />
+    <p>Olhe a aba do navegador ☝</p>
+    <p>A aplicação agora tem um título 🙋</p>
+  </div>
 )
