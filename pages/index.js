7bf015876277b440ac765e87808e46ef8f1ef842
Josias Iquabius <iquabius@gmail.com>
2017-04-27 14:32:10 -0400

    Refactor: Add Layout comp. with shared elements
    Com o componente Layout é possível reutilizar elementos compartilhados
    entre várias páginas, como a barra de navegação e o rodapé.

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,10 @@
-import Head from 'next/head'
-import Link from 'next/link'
+import Layout from '../components/layout'
 
 export default () => (
-  <div>
-    <Head>
-      <title>Olimat 🔥</title>
-      <meta charSet='utf-8' />
-      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-    </Head>
-
-    <header>
-      <nav>
-        <Link href='/'><a>Home</a></Link> |
-        <Link href='/questions'><a>Questões</a></Link> |
-      </nav>
-    </header>
-
+  <Layout>
     <p>Olá, esta é a aplicação 'frontend' para o projeto Olimat!</p>
     <hr />
     <p>Olhe a aba do navegador ☝</p>
     <p>A aplicação agora tem um título 🙋</p>
-  </div>
+  </Layout>
 )
--- pages/questions/index.js
+++ pages/questions/index.js
@@ -1,21 +1,8 @@
-import Head from 'next/head'
-import Link from 'next/link'
+import Layout from '../../components/layout'
 
 export default () => (
-  <div>
-    <Head>
-      <title>Olimat 🔥</title>
-      <meta charSet='utf-8' />
-      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-    </Head>
-    <header>
-      <nav>
-        <Link href='/'><a>Home</a></Link> |
-        <Link href='/questions'><a>Questões</a></Link> |
-      </nav>
-    </header>
-
+  <Layout>
     <h1>Questões</h1>
     <p>Página para listar as Questões.</p>
-  </div>
+  </Layout>
 )
