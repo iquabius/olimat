c08a7de2bfa8a9b0a365b80b94a562f3ef37e835
Josias Duarte Busiquia <iquabius@gmail.com>
2017-05-16 17:08:46 -0400

    Feat(questions): Add sample questions
    Essas amostras serão utilizadas até que uma camada de persistência
    (com banco de dados), seja implementada.

---
+++ data/questions.js
@@ -0,0 +1,32 @@
+const questions = [
+  {
+    id: 1,
+    verified_by: [],
+    wording: 'Quanto é 3+3?',
+    image: '',
+    choices: [0, 6, 3, 9],
+    answer: 6,
+    including_tests: [],
+    created_by: '',
+    created_at: '',
+    modified_at: '',
+    title: 'Adição de Inteiros',
+    tags: []
+  },
+  {
+    id: 2,
+    verified_by: [],
+    wording: 'Quanto é 5-3?',
+    image: '',
+    choices: [15, 8, 2, -2],
+    answer: 2,
+    including_tests: [],
+    created_by: '',
+    created_at: '',
+    modified_at: '',
+    title: 'Subtração de Inteiros',
+    tags: []
+  }
+]
+
+export default questions
