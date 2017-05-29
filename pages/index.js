eb2d20887ff61a2f29989b6bdec9c02b6a2309d5
Josias Duarte Busiquia <iquabius@gmail.com>
2017-05-29 18:55:03 -0400

    Refactor: Rename 'Layout' to 'App'

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import Layout from '../components/layout'
+import App from '../components/App'
 
 export default () => (
-  <Layout>
+  <App>
     <p>Olá, esta é a aplicação 'frontend' para o projeto Olimat!</p>
     <hr />
     <p>Olhe a aba do navegador ☝</p>
     <p>A aplicação agora tem um título 🙋</p>
-  </Layout>
+  </App>
 )
