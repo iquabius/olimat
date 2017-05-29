1ff2138617bdf9d18a6ddad89df5da903cf1bec7
Josias Iquabius <iquabius@gmail.com>
2017-05-29 19:04:28 -0400

    Refactor: Convert App from function to class

--- components/App.js
+++ components/App.js
@@ -1,26 +1,33 @@
+import { Component } from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
 
-export default ({ children, title = 'Olimat 🔥' }) => (
-  <div>
-    <Head>
-      <title>{ title }</title>
-      <meta charSet='utf-8' />
-      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-    </Head>
-    <header>
-      <nav>
-        <Link href='/'><a>Home</a></Link> |
-        <Link href='/questions'><a>Questões</a></Link> |
-      </nav>
-    </header>
+export default class App extends Component {
+  render () {
+    let { children, title = 'Olimat 🔥' } = this.props
 
-    { children }
+    return (
+      <div>
+        <Head>
+          <title>{ title }</title>
+          <meta charSet='utf-8' />
+          <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        </Head>
+        <header>
+          <nav>
+            <Link href='/'><a>Home</a></Link> |
+            <Link href='/questions'><a>Questões</a></Link> |
+          </nav>
+        </header>
 
-    <footer>
-      <hr />
-      Esse é o rodapé. Ele aparece em todas as páginas que usarem o componente
-      Layout, assim como os links de navegação no cabeçalho.
-    </footer>
-  </div>
-)
+        { children }
+
+        <footer>
+          <hr />
+          Esse é o rodapé. Ele aparece em todas as páginas que usarem o componente
+          Layout, assim como os links de navegação no cabeçalho.
+        </footer>
+      </div>
+    )
+  }
+}
