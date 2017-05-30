022333d39e0b75a16b5a6206b62cb2d0723006ae
Josias Iquabius <iquabius@gmail.com>
2017-05-30 17:11:49 -0400

    Feat(admin): Add AppBar to DashboardFrame

--- components/DashboardFrame.js
+++ components/DashboardFrame.js
@@ -2,7 +2,27 @@ import { Component } from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
 import PropTypes from 'prop-types'
+import AppBar from 'material-ui/AppBar'
+import Toolbar from 'material-ui/Toolbar'
+import Typography from 'material-ui/Typography'
+import IconButton from 'material-ui/IconButton'
+import MenuIcon from 'material-ui-icons/Menu'
+import MoreVert from 'material-ui-icons/MoreVert'
 import App from './App'
+
+const styles = {
+  dashboardFrame: {
+    position: 'relative',
+    width: '100%'
+  },
+  appBar: {
+    backgroundColor: '#063d01',
+    position: 'relative'
+  },
+  flex: {
+    flex: 1
+  }
+}
 
 class DashboardFrame extends Component {
   render () {
@@ -15,12 +35,19 @@ class DashboardFrame extends Component {
           <meta charSet='utf-8' />
           <meta name='viewport' content='initial-scale=1.0, width=device-width' />
         </Head>
-        <header>
-          <nav>
-            <Link href='/'><a>Home</a></Link> |
-            <Link href='/questions'><a>Questões</a></Link> |
-          </nav>
-        </header>
+        <div style={styles.dashboardFrame}>
+          <AppBar style={styles.appBar}>
+            <Toolbar>
+              <IconButton contrast>
+                <MenuIcon />
+              </IconButton>
+              <Typography type='title' colorInherit style={styles.flex}>Olimat</Typography>
+              <IconButton contrast>
+                <MoreVert />
+              </IconButton>
+            </Toolbar>
+          </AppBar>
+        </div>
 
         { children }
 
