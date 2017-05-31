8015fae24c76e0a8668a848a85e6b3dca07d611c
Josias Iquabius <iquabius@gmail.com>
2017-05-31 00:26:00 -0400

    Refactor: Remove duplicated <meta> tags
    O elemento <title> está declarado em 'components/App.js', e os elementos
    <meta>, para 'charset' e 'viewport', estão declaradas em
    'pages/_document.js'.

--- components/App.js
+++ components/App.js
@@ -43,8 +43,6 @@ class App extends Component {
         <AppWrapper>
           <Head>
             <title>{ title }</title>
-            <meta charSet='utf-8' />
-            <meta name='viewport' content='initial-scale=1.0, width=device-width' />
           </Head>
 
           { children }
