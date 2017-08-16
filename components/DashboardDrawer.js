d5be0f970151ee48c5816bcbde6a3ad4cbd0fd15
Josias Iquabius <iquabius@gmail.com>
2017-08-16 03:28:50 -0400

    Feat(admin): add main links to DashboardDrawer

--- components/DashboardDrawer.js
+++ components/DashboardDrawer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Drawer from 'material-ui/Drawer'
 import List, { ListItem, ListItemText } from 'material-ui/List'
+import Divider from 'material-ui/Divider'
 import Link from 'next/link'
 
 const styles = {
@@ -25,6 +26,40 @@ export default class DashboardDrawer extends React.Component {
             onClick={this.props.onRequestClose}
           >
             <ListItemText primary='Questões' />
+          </ListItem>
+
+          <ListItem
+            component='a'
+            href='/dashboard/tests'
+            onClick={this.props.onRequestClose}
+          >
+            <ListItemText primary='Provas' />
+          </ListItem>
+
+          <Divider />
+
+          <ListItem
+            component='a'
+            href='/dashboard/editions'
+            onClick={this.props.onRequestClose}
+          >
+            <ListItemText primary='Edições' />
+          </ListItem>
+
+          <ListItem
+            component='a'
+            href='/dashboard/cities'
+            onClick={this.props.onRequestClose}
+          >
+            <ListItemText primary='Cidades' />
+          </ListItem>
+
+          <ListItem
+            component='a'
+            href='/dashboard/regions'
+            onClick={this.props.onRequestClose}
+          >
+            <ListItemText primary='Regiões' />
           </ListItem>
         </List>
       </Drawer>
