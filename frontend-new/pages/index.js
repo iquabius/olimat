7b0fc27478489b47ccfdcd57717871c0d8bcac90
Josias Duarte Busiquia <iquabius@gmail.com>
2017-08-16 06:30:18 -0400

    Refactor: move home page to new frontend setup

---
+++ frontend-new/pages/index.js
@@ -0,0 +1,10 @@
+import AppFrame from '../components/AppFrame'
+
+export default () => (
+  <AppFrame>
+    <p>Olá, esta é a aplicação 'frontend' para o projeto Olimat!</p>
+    <hr />
+    <p>Olhe a aba do navegador ☝</p>
+    <p>A aplicação agora tem um título 🙋</p>
+  </AppFrame>
+)
