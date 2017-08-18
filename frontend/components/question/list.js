a01c3cd03f91df6ebbfe830b9d9175a8ccc0e60a
Josias Iquabius <iquabius@gmail.com>
2017-08-17 20:59:15 -0400

    Is the questions coming from the backend database?

--- frontend/components/question/list.js
+++ frontend/components/question/list.js
@@ -1,4 +1,6 @@
 import {Component} from 'react'
+// The questions are not coming from the database yet, they're currently being
+// statically loaded as JSON data from 'frontend/data/questions.js'.
 import questions from '../../data/questions'
 import withController from '../../components/withController'
 
