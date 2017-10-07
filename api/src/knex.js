2aa38ef3e5927a02dd4d59217fc4d76d44e621e0
Josias Iquabius <iquabius@gmail.com>
2017-10-07 00:35:38 -0400

    Add TODO notes to knex connection file

--- api/src/knex.js
+++ api/src/knex.js
@@ -5,7 +5,10 @@ const knex = require('knex')
 
 module.exports = function () {
   const app = this
+  // TODO check if database is ready before trying to connect
+  //   or try again after some time (try/catch)
   const db = knex(knexConfig)
 
+  // TODO run migrations in development mode
   app.set('knexClient', db)
 }
