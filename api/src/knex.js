e5bd439981e8dda2309e7a2abd309ae0d462198e
Josias Iquabius <iquabius@gmail.com>
2017-10-13 15:54:47 -0400

    Run migrations and seeding in development mode

--- api/src/knex.js
+++ api/src/knex.js
@@ -6,7 +6,23 @@ module.exports = function () {
   const app = this
   // TODO check if database is ready before trying to connect
   //   or try again after some time (try/catch)
+  console.log('postgres: Connecting to the database...')
   const db = knex(knexConfig)
+
+  if (process.env.NODE_ENV === 'development') {
+    console.log('postgres: Trying to migrate database with Knex')
+    db.migrate.latest()
+      .then(() => {
+        console.log('postgres: Migration run successfully')
+        console.log('postgres: Trying to seed the database now')
+        return db.seed.run()
+          .then(() => {
+            console.log('postgres: Database seeding run successfully')
+          }).catch(error =>
+                   console.error('postgres: Error seeding the database', error))
+      }).catch(error =>
+               console.error('postgres: Error trying to migrate the database', error))
+  }
 
   // TODO run migrations in development mode
   app.set('knexClient', db)
