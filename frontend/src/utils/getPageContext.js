c3d370cc2a0f88a7e3c3d0ad706b4ab8980d7a69
Josias Iquabius <iquabius@gmail.com>
2018-02-20 11:27:12 -0400

    Change theme color to green and blue

--- frontend/src/utils/getPageContext.js
+++ frontend/src/utils/getPageContext.js
@@ -2,22 +2,22 @@
 
 import { SheetsRegistry } from 'jss';
 import { createMuiTheme, createGenerateClassName } from 'material-ui/styles';
-import purple from 'material-ui/colors/purple';
 import green from 'material-ui/colors/green';
+import blue from 'material-ui/colors/blue';
 
 // A theme with custom primary and secondary color.
 // It's optional.
 const theme = createMuiTheme({
   palette: {
     primary: {
-      light: purple[300],
-      main: purple[500],
-      dark: purple[700],
+      light: green[500],
+      main: green[800],
+      dark: green[900],
     },
     secondary: {
-      light: green[300],
-      main: green[500],
-      dark: green[700],
+      light: blue[300],
+      main: blue[500],
+      dark: blue[700],
     },
   },
 });
