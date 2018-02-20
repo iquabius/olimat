81f6d97c60ff7109ccef7a3fbd570a96e36a31ac
Josias Iquabius <iquabius@gmail.com>
2018-02-20 11:57:07 -0400

    Add favicon.ico
    Essa arte foi encontrada no portal do campus de Sinop.
    
    http://sinop.unemat.br/site/projetos-da-unemat/olimpiadas-de-matematica-da-unemat-campus-sinop/

--- frontend/src/pages/_document.js
+++ frontend/src/pages/_document.js
@@ -26,6 +26,7 @@ class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
           />
+          <link rel="shortcut icon" href="/static/favicon.ico" />
         </Head>
         <body>
           <Main />
