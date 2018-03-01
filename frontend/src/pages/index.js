83ae2643c8c82283474061dd633f51b53620d261
Josias Iquabius <iquabius@gmail.com>
2018-03-01 16:47:05 -0400

    Add homepage from Materil-UI's documentation app

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,76 +1,126 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Button from 'material-ui/Button';
-import Dialog, {
-  DialogTitle,
-  DialogContent,
-  DialogContentText,
-  DialogActions,
-} from 'material-ui/Dialog';
-import Typography from 'material-ui/Typography';
+import compose from 'recompose/compose';
+import Head from 'next/head';
 import { withStyles } from 'material-ui/styles';
+import Typography from 'material-ui/Typography';
+import Button from 'material-ui/Button';
 import withRoot from '../utils/withRoot';
 import AppFrame from '../components/AppFrame';
+import AppFooter from '../components/AppFooter';
+import Link from '../components/Link';
 
 const styles = theme => ({
   root: {
+    flex: '1 0 100%',
+  },
+  hero: {
+    minHeight: '95vh',
+    flex: '0 0 auto',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor:
+      theme.palette.type === 'light' ? theme.palette.primary.main : theme.palette.primary.dark,
+    color: theme.palette.primary.contrastText,
+  },
+  content: {
+    paddingTop: theme.spacing.unit * 8,
+    paddingBottom: theme.spacing.unit * 8,
+    [theme.breakpoints.up('sm')]: {
+      paddingTop: theme.spacing.unit * 12,
+      paddingBottom: theme.spacing.unit * 10,
+    },
+  },
+  text: {
+    paddingLeft: theme.spacing.unit * 4,
+    paddingRight: theme.spacing.unit * 4,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  headline: {
+    maxWidth: 500,
     textAlign: 'center',
-    paddingTop: theme.spacing.unit * 20,
+  },
+  button: {
+    marginTop: theme.spacing.unit * 3,
+  },
+  logo: {
+    margin: '20px 0',
+    width: '100%',
+    height: '35vw',
+    maxHeight: 200,
+  },
+  backers: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing.unit * 2,
+    display: 'flex',
+    justifyContent: 'center',
+    minHeight: 600,
+  },
+  backersBody: {
+    maxWidth: theme.spacing.unit * 90,
   },
 });
 
-class Index extends React.Component {
-  state = {
-    open: false,
-  };
+function PageHome(props) {
+  const classes = props.classes;
 
-  handleClose = () => {
-    this.setState({
-      open: false,
-    });
-  };
-
-  handleClick = () => {
-    this.setState({
-      open: true,
-    });
-  };
-
-  render() {
-    const { classes } = this.props;
-    const { open } = this.state;
-
-    return (
-      <AppFrame>
-        <div className={classes.root}>
-          <Dialog open={open} onClose={this.handleClose}>
-            <DialogTitle>Super Secret Password</DialogTitle>
-            <DialogContent>
-              <DialogContentText>1-2-3-4-5</DialogContentText>
-            </DialogContent>
-            <DialogActions>
-              <Button color="primary" onClick={this.handleClose}>
-                OK
+  return (
+    <AppFrame>
+      <div className={classes.root}>
+        <Head>
+          <title>OliMAT</title>
+        </Head>
+        <div className={classes.hero}>
+          <div className={classes.content}>
+            <img
+              src="/static/images/material-ui-logo.svg"
+              alt="Material-UI Logo"
+              className={classes.logo}
+            />
+            <div className={classes.text}>
+              <Typography variant="display2" component="h1" color="inherit" gutterBottom>
+                {'Olimpíadas de Matemática da UNEMAT'}
+              </Typography>
+              <Typography
+                variant="headline"
+                component="h2"
+                color="inherit"
+                className={classes.headline}
+              >
+                {"Inscrições abertas de 21 de fevereiro a 2 de abril."}
+              </Typography>
+              <Button
+                component={buttonProps => (
+                  <Link
+                    variant="button"
+                    prefetch
+                    href="/getting-started/installation"
+                    {...buttonProps}
+                  />
+                )}
+                className={classes.button}
+                variant="raised"
+              >
+                {'Inscreva-se'}
               </Button>
-            </DialogActions>
-          </Dialog>
-          <Typography type="display1" gutterBottom>
-            Material-UI
-          </Typography>
-          <Typography type="subheading" gutterBottom>
-            example project
-          </Typography>
-          <Button variant="raised" color="secondary" onClick={this.handleClick}>
-            Super Secret Password
-          </Button>
+            </div>
+          </div>
         </div>
-      </AppFrame>
-    );
-  }
+        <div className={classes.backers}>
+          Here were the NoSSR and MarkdownElement components...
+        </div>
+        <AppFooter />
+      </div>
+    </AppFrame>
+  );
 }
 
-Index.propTypes = {
+PageHome.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withRoot(withStyles(styles)(Index));
+export default compose(withRoot, withStyles(styles))(PageHome);
