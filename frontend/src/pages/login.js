558e015e6783a9bd95cc522796b18b44732dcdba
Josias Iquabius <iquabius@gmail.com>
2018-03-01 12:26:34 -0400

    Create OnlyFormFrame for login and signup pages

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,63 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import withRoot from '../utils/withRoot';
-import { withStyles } from 'material-ui/styles';
 import Head from 'next/head';
+import OnlyFormFrame from '../components/OnlyFormFrame';
 import LoginForm from '../components/LoginForm';
 
-const styles = theme => ({
-  loginContent: {
-    minHeight: '100vh',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  halfBg: {
-    height: '100vh',
-    backgroundColor: theme.palette.grey[300],
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    zIndex: -1,
-    '& div': {
-      backgroundColor: theme.palette.primary.main,
-      height: '50vh',
-    }
-  },
-  logo: {
-    fontFamily: theme.typography.fontFamily,
-    color: theme.palette.primary.contrastText,
-    '& h1': {
-      fontSize: theme.typography.display1.fontSize,
-      fontWeight: 400,
-    }
-  },
-});
-
 function PageLogin(props) {
-  const { classes } = props;
   return (
-  <React.Fragment>
-    <Head>
-      <title>Login - OliMAT</title>
-    </Head>
-    <section className={classes.halfBg}>
-      <div></div>
-    </section>
-    <section className={classes.loginContent}>
-      <div className={classes.logo}>
-        <h1>OliMAT</h1>
-      </div>
+    <OnlyFormFrame>
+      <Head>
+        <title>Login - OliMAT</title>
+      </Head>
       <LoginForm />
-    </section>
-  </React.Fragment>
-)};
+    </OnlyFormFrame>
+  );
+};
 
 PageLogin.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withRoot(withStyles(styles)(PageLogin));
+export default withRoot(PageLogin);
