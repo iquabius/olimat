af0061fd8f6b337b498c488edfd760e3e5412a9c
Josias Iquabius <iquabius@gmail.com>
2018-03-03 17:26:26 -0400

    Add login feature

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { graphql, withApollo, compose } from 'react-apollo';
+import cookie from 'cookie';
+import gql from 'graphql-tag';
 import { withStyles } from 'material-ui/styles';
 import IconButton from 'material-ui/IconButton';
 import Input, { InputLabel, InputAdornment } from 'material-ui/Input';
@@ -13,6 +16,8 @@ import Button from 'material-ui/Button';
 import Link from '../components/Link';
 import Visibility from 'material-ui-icons/Visibility';
 import VisibilityOff from 'material-ui-icons/VisibilityOff';
+
+import redirect from '../utils/redirect'
 
 const styles = theme => ({
   loginBox: {
@@ -68,57 +73,62 @@ class LoginForm extends React.Component {
           Acesse sua Conta
         </Typography>
         <Divider />
-        <TextField
-          label="Email"
-          margin="normal"
-          fullWidth
-          onChange={this.handleChange('email')}
-        />
-        <FormControl fullWidth margin="normal">
-          <InputLabel htmlFor="password">Senha</InputLabel>
-          <Input
-            inputProps={{className: classes.passwordInput}}
-            type={this.state.showPassword ? 'text' : 'password'}
-            value={this.state.password}
-            onChange={this.handleChange('password')}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  style={{width: 'auto'}}
-                  onClick={this.handleClickShowPasssword}
-                  onMouseDown={this.handleMouseDownPassword}
-                >
-                  {this.state.showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
+        <form onSubmit={this.props.signin}>
+          <TextField
+            name="email"
+            label="Email"
+            margin="normal"
+            fullWidth
+            onChange={this.handleChange('email')}
           />
-        </FormControl>
-        <FormGroup row>
-          <FormControlLabel
-            control={
-              <Switch
-                checked={this.state.keepLoggedIn}
-                onChange={this.handleCheckbox}
-                value="keepLoggedIn"
-                color="primary"
-              />
-            }
-            label="Manter acesso"
-          />
-        </FormGroup>
-        {/* <Typography variant="caption">
-          <Link href="#">Esqueceu a senha?</Link>
-        </Typography> */}
-        <Button
-          className={classes.loginButton}
-          fullWidth 
-          variant="raised"
-          color="secondary"
-          size="large"
-        >
-          Entrar
-        </Button>
+          <FormControl fullWidth margin="normal">
+            <InputLabel htmlFor="password">Senha</InputLabel>
+            <Input
+              name="password"
+              inputProps={{className: classes.passwordInput}}
+              type={this.state.showPassword ? 'text' : 'password'}
+              value={this.state.password}
+              onChange={this.handleChange('password')}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    style={{width: 'auto'}}
+                    onClick={this.handleClickShowPasssword}
+                    onMouseDown={this.handleMouseDownPassword}
+                  >
+                    {this.state.showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+          <FormGroup row>
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={this.state.keepLoggedIn}
+                  onChange={this.handleCheckbox}
+                  value="keepLoggedIn"
+                  color="primary"
+                />
+              }
+              label="Manter acesso"
+            />
+          </FormGroup>
+          {/* <Typography variant="caption">
+            <Link href="#">Esqueceu a senha?</Link>
+          </Typography> */}
+          <Button
+            className={classes.loginButton}
+            fullWidth 
+            variant="raised"
+            color="secondary"
+            size="large"
+            type="submit"
+          >
+            Entrar
+          </Button>
+        </form>
         <Typography
           className={classes.helpMessage}
           variant="caption"
@@ -131,4 +141,61 @@ class LoginForm extends React.Component {
   }
 }
 
-export default withStyles(styles)(LoginForm);
+export default compose(
+  withStyles(styles),
+  // withApollo exposes `this.props.client` used when logging out
+  withApollo,
+  graphql(
+    // The `login` mutation is provided by the backend
+    gql`
+      mutation Signin($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+          token
+        }
+      }
+    `,
+    {
+      // Use an unambiguous name for use in the `props` section below
+      name: 'signinWithEmail',
+      // Apollo's way of injecting new props which are passed to the component
+      props: ({
+        signinWithEmail,
+        // `client` is provided by the `withApollo` HOC
+        ownProps: { client }
+      }) => ({
+        // `signin` is the name of the prop passed to the component
+        signin: event => {
+          /* global FormData */
+          const data = new FormData(event.target)
+          console.log(data)
+
+          event.preventDefault()
+          event.stopPropagation()
+
+          signinWithEmail({
+            variables: {
+              email: data.get('email'),
+              password: data.get('password')
+            }
+          }).then(({ data: { login: { token } } }) => {
+            // Store the token in cookie
+            document.cookie = cookie.serialize('token', token, {
+              maxAge: 30 * 24 * 60 * 60 // 30 days
+            })
+
+            // Force a reload of all the current queries now that the user is
+            // logged in
+            client.resetStore().then(() => {
+              // Now redirect to the homepage
+              redirect({}, '/')
+            })
+          }).catch((error) => {
+            // Something went wrong, such as incorrect password, or no network
+            // available, etc.
+            console.error(error)
+          })
+        }
+      })
+    }
+  )
+)(LoginForm);
--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { compose } from 'react-apollo';
 import withRoot from '../utils/withRoot';
 import Head from 'next/head';
 import OnlyFormFrame from '../components/OnlyFormFrame';
 import LoginForm from '../components/LoginForm';
+import withData from '../utils/withData';
+import checkLoggedIn from '../utils/checkLoggedIn'
+import redirect from '../utils/redirect'
 
 function PageLogin(props) {
   return (
@@ -16,8 +20,21 @@ function PageLogin(props) {
   );
 };
 
-PageLogin.propTypes = {
-  classes: PropTypes.object.isRequired,
+PageLogin.getInitialProps = async (context, apolloClient) => {
+  const { loggedInUser } = await checkLoggedIn(context, apolloClient)
+
+  // if (loggedInUser.user) {
+  if (loggedInUser.me) {
+    // Already signed in? No need to continue.
+    // Throw them back to the main page
+    redirect(context, '/')
+  }
+
+  return {}
 };
 
-export default withRoot(PageLogin);
+export default compose(
+  withRoot,
+  // withData gives us server-side graphql queries before rendering
+  withData,
+)(PageLogin);
