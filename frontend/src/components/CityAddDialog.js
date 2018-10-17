79a260fb3664bb801696044f4ece2691e2177d5a
Josias Iquabius <iquabius@gmail.com>
2018-10-17 13:28:07 -0400

    Refactor City dialog to use render props
    Give preference to Render Props instead of Higher
    Order Functions

--- frontend/src/components/CityAddDialog.js
+++ frontend/src/components/CityAddDialog.js
@@ -3,58 +3,10 @@ import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import { Dialog, DialogActions, DialogContent, DialogTitle } from '@material-ui/core';
-import { graphql, compose } from 'react-apollo';
+import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
-import { withFormik } from 'formik';
+import { Formik } from 'formik';
 import { allCitiesQuery } from './CityList';
-
-const CityAddDialog = ({
-  open,
-  onClose,
-  handleSubmit,
-  handleChange,
-  handleBlur,
-  isSubmitting,
-  values,
-}) => (
-  <Dialog open={open} onClose={onClose} aria-labelledby="city-add-dialog">
-    <DialogTitle id="city-add-dialog">Adicione uma cidade</DialogTitle>
-    <form onSubmit={handleSubmit}>
-      <DialogContent>
-        <TextField
-          name="name"
-          autoFocus
-          margin="dense"
-          label="Nome"
-          fullWidth
-          value={values.name}
-          onChange={handleChange}
-          onBlur={handleBlur}
-        />
-      </DialogContent>
-      <DialogActions>
-        <Button disabled={isSubmitting} onClick={onClose} color="primary">
-          Cancelar
-        </Button>
-        <Button disabled={isSubmitting} type="submit" color="primary">
-          Adicionar
-        </Button>
-      </DialogActions>
-    </form>
-  </Dialog>
-);
-
-CityAddDialog.propTypes = {
-  handleBlur: PropTypes.func.isRequired,
-  handleChange: PropTypes.func.isRequired,
-  handleSubmit: PropTypes.func.isRequired,
-  isSubmitting: PropTypes.bool.isRequired,
-  onClose: PropTypes.func.isRequired,
-  open: PropTypes.bool.isRequired,
-  values: PropTypes.shape({
-    name: PropTypes.string.isRequired,
-  }).isRequired,
-};
 
 export const newCityMutation = gql`
   mutation newCityMutation($name: String!) {
@@ -65,38 +17,72 @@ export const newCityMutation = gql`
   }
 `;
 
-export default compose(
-  graphql(newCityMutation, {
-    // Use an unambiguous name for use in the `props` section below
-    name: 'newCity',
-  }),
-  withFormik({
-    mapPropsToValues: () => ({
-      name: '',
-    }),
-    handleSubmit: (values, { props: { newCity, onClose }, setSubmitting }) => {
-      newCity({
-        variables: {
-          name: values.name,
-        },
-        update: (proxy, { data: { createCity } }) => {
-          const data = proxy.readQuery({ query: allCitiesQuery });
-          data.cities.push(createCity);
-
-          proxy.writeQuery({ query: allCitiesQuery, data });
-        },
-      })
-        .then(response => {
-          console.log(`Mutation response: `);
-          console.log(response);
-          setSubmitting(false);
-          onClose();
-        })
-        .catch(error => {
-          // Something went wrong, such as incorrect password, or no network
-          // available, etc.
-          console.error(error);
-        });
+const onSubmitCity = (newCity, onClose) => (values, { resetForm }) => {
+  newCity({
+    variables: {
+      name: values.name,
     },
-  }),
-)(CityAddDialog);
+  })
+    .then(response => {
+      console.log(`Mutation response: `);
+      console.log(response);
+      resetForm();
+      onClose();
+    })
+    .catch(error => {
+      // Something went wrong, such as incorrect password, or no network
+      // available, etc.
+      console.error(error);
+    });
+};
+
+const CityAddDialog = ({ open, onClose }) => (
+  <Mutation
+    mutation={newCityMutation}
+    update={(proxy, { data: { createCity } }) => {
+      const data = proxy.readQuery({ query: allCitiesQuery });
+      data.cities.push(createCity);
+
+      proxy.writeQuery({ query: allCitiesQuery, data });
+    }}
+  >
+    {(newCity, { data }) => (
+      <Formik initialValues={{ name: '' }} onSubmit={onSubmitCity(newCity, onClose)}>
+        {({ handleSubmit, handleChange, handleBlur, isSubmitting, values }) => (
+          <Dialog open={open} onClose={onClose} aria-labelledby="city-add-dialog">
+            <DialogTitle id="city-add-dialog">Adicione uma cidade</DialogTitle>
+            <form onSubmit={handleSubmit}>
+              <DialogContent>
+                <TextField
+                  name="name"
+                  autoFocus
+                  margin="dense"
+                  label="Nome"
+                  fullWidth
+                  value={values.name}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+              </DialogContent>
+              <DialogActions>
+                <Button disabled={isSubmitting} onClick={onClose} color="primary">
+                  Cancelar
+                </Button>
+                <Button disabled={isSubmitting} type="submit" color="primary">
+                  Adicionar
+                </Button>
+              </DialogActions>
+            </form>
+          </Dialog>
+        )}
+      </Formik>
+    )}
+  </Mutation>
+);
+
+CityAddDialog.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  open: PropTypes.bool.isRequired,
+};
+
+export default CityAddDialog;
