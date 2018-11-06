fc1f89ec9d1faf3a5714fe9600f2b35cea5898ee
Josias Iquabius <iquabius@gmail.com>
2018-11-06 20:53:44 -0300

    Refactor FAButton into own component

---
+++ frontend/src/components/FAButton.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles, Button } from '@material-ui/core';
+
+const styles = theme => ({
+  FAButton: {
+    position: 'absolute',
+    top: -28,
+    left: theme.spacing.unit * -6,
+  },
+  [theme.breakpoints.down(1000 + theme.spacing.unit * 8)]: {
+    FAButton: {
+      position: 'fixed',
+      top: 'auto',
+      left: 'auto',
+      bottom: 20,
+      right: 20,
+    },
+  },
+});
+
+const FAButton = ({ children, classes, onClick, ...rest }) => (
+  <Button onClick={onClick} variant="fab" color="secondary" className={classes.FAButton} {...rest}>
+    {children}
+  </Button>
+);
+
+export default withStyles(styles)(FAButton);
--- frontend/src/components/Question/List.js
+++ frontend/src/components/Question/List.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles, Button } from '@material-ui/core';
+import { withStyles } from '@material-ui/core';
 import ListItem from './ListItem';
 import ListConnector from './ListConnector';
 import CreateDialog from './CreateDialog';
+import FAButton from '../FAButton';
 import AddIcon from '@material-ui/icons/Add';
 
 const styles = theme => ({
@@ -14,20 +15,6 @@ const styles = theme => ({
   grid: {
     display: 'flex',
     flexWrap: 'wrap',
-  },
-  FAButton: {
-    position: 'absolute',
-    top: -28,
-    left: theme.spacing.unit * -6,
-  },
-  [theme.breakpoints.down(1000 + theme.spacing.unit * 8)]: {
-    FAButton: {
-      position: 'fixed',
-      top: 'auto',
-      left: 'auto',
-      bottom: 20,
-      right: 20,
-    },
   },
 });
 
@@ -46,15 +33,9 @@ class QuestionList extends React.Component {
 
     return (
       <React.Fragment>
-        <Button
-          onClick={this.handleOpenCreateDialog}
-          variant="fab"
-          color="secondary"
-          aria-label="Adicionar questão"
-          className={classes.FAButton}
-        >
+        <FAButton onClick={this.handleOpenCreateDialog} aria-label="Adicionar questão">
           <AddIcon />
-        </Button>
+        </FAButton>
         <CreateDialog open={createDialogOpen} onClose={this.handleCloseCreateDialog} />
         <ListConnector>
           {({ allQuestions }) => (
