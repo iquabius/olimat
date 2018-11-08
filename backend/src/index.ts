31c39f5f8650899449e3c480bc03c05616cb472e
Josias Iquabius <iquabius@gmail.com>
2018-11-07 21:35:51 -0300

    Add express.static route to serve image files

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import { GraphQLServer } from 'graphql-yoga';
 import { prisma } from './generated/prisma-client';
 import resolvers from './resolvers';
+const express = require('express');
 
 const server = new GraphQLServer({
   typeDefs: 'src/schema.graphql',
@@ -10,5 +11,8 @@ const server = new GraphQLServer({
     db: prisma,
   }),
 });
+
+// The 'files' directory should be a docker volume, maybe in a dedicated container
+server.express.use('/files', express.static('files'));
 
 server.start(() => console.log('Server is running on http://localhost:4000'));
