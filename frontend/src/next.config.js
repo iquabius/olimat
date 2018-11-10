805a5ae93759128c82548f72b23e534e3e5bf9a8
Josias Iquabius <iquabius@gmail.com>
2018-11-09 23:44:34 -0300

    Add @zeit/next-css plugin (required by filepond)

--- frontend/src/next.config.js
+++ frontend/src/next.config.js
@@ -1,3 +1,6 @@
-module.exports = {
-  distDir: '../.next',
-};
+const withCSS = require('@zeit/next-css');
+
+// Use next-compose-plugins if this gets weird
+module.exports = withCSS({
+  distDir: '../.next', // this is next's config, not next-css'
+});
