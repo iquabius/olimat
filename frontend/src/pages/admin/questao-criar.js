bb2e795611c75a8b1336232119fc3b46bf7825f1
Josias Iquabius <iquabius@gmail.com>
2018-11-11 18:40:51 -0300

    Remove withRouter from PageQuestionCreate
    It doesn't need the router. It there because of
    copy & paste.

--- frontend/src/pages/admin/questao-criar.js
+++ frontend/src/pages/admin/questao-criar.js
@@ -1,25 +1,15 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import withRoot from '../../utils/withRoot';
 import AppFrame from '../../components/AppFrame';
 import AppContent from '../../components/AppContent';
 import QuestionCreateForm from '../../components/Question/CreateForm';
-import { compose } from 'recompose';
-import { withRouter } from 'next/router';
 
-const PageQuestionCreate = ({ router }) => (
+const PageQuestionCreate = () => (
   <AppFrame>
     <AppContent>
-      <QuestionCreateForm id={router.query.id} />
+      <QuestionCreateForm />
     </AppContent>
   </AppFrame>
 );
 
-PageQuestionCreate.propTypes = {
-  router: PropTypes.object.isRequired,
-};
-
-export default compose(
-  withRoot,
-  withRouter,
-)(PageQuestionCreate);
+export default withRoot(PageQuestionCreate);
