68ab08449112901e3495e339b597e8bcc44da160
Josias Iquabius <iquabius@gmail.com>
2018-11-12 14:01:39 -0300

    Add link to create question page go back to list

--- frontend/src/components/Question/CreateForm.js
+++ frontend/src/components/Question/CreateForm.js
@@ -4,16 +4,26 @@ import CreateConnector from './CreateConnector';
 import QuestionForm from './Form';
 import { formValuesToRequest } from './transforms';
 import { questionInitialValues, createHandleSubmit } from './CreateDialog';
+import NextLink from 'next/link';
+import FAButton from '../FAButton';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 
 const QuestionCreateForm = () => (
-  <CreateConnector>
-    {({ createQuestion }) => (
-      <QuestionForm
-        initialValues={questionInitialValues}
-        onSubmit={createHandleSubmit(createQuestion)}
-      />
-    )}
-  </CreateConnector>
+  <React.Fragment>
+    <NextLink href={`/admin/questoes`}>
+      <FAButton aria-label="Voltar pra lista de questões">
+        <ArrowBackIcon />
+      </FAButton>
+    </NextLink>
+    <CreateConnector>
+      {({ createQuestion }) => (
+        <QuestionForm
+          initialValues={questionInitialValues}
+          onSubmit={createHandleSubmit(createQuestion)}
+        />
+      )}
+    </CreateConnector>
+  </React.Fragment>
 );
 
 export default QuestionCreateForm;
