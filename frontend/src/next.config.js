326b22869b3782fc6be198fc58119487b0295f0d
Josias Iquabius <iquabius@gmail.com>
2018-11-11 23:34:44 -0300

    Add webpack-bundle-analyzer to Next.js app

--- frontend/src/next.config.js
+++ frontend/src/next.config.js
@@ -1,6 +1,23 @@
 const withCSS = require('@zeit/next-css');
 
+const { ANALYZE } = process.env;
+
 // Use next-compose-plugins if this gets weird
 module.exports = withCSS({
   distDir: '../.next', // this is next's config, not next-css'
+  webpack: (config, { isServer }) => {
+    if (ANALYZE) {
+      const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+      config.plugins.push(
+        new BundleAnalyzerPlugin({
+          analyzerMode: 'server',
+          analyzerPort: isServer ? 8888 : 8889,
+          openAnalyzer: true,
+        }),
+      );
+    }
+
+    return config;
+  },
 });
