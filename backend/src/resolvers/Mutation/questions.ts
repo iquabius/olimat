2e7a3febbfa5a2919f419f880e983f43f174b1e3
Josias Iquabius <iquabius@gmail.com>
2018-11-13 20:45:56 -0300

    Fix updateQuestion mutation to handle replacing image

--- backend/src/resolvers/Mutation/questions.ts
+++ backend/src/resolvers/Mutation/questions.ts
@@ -1,4 +1,5 @@
 import { Context } from '../../utils';
+import * as fs from 'fs';
 import * as mv from 'mv';
 import * as path from 'path';
 // import { Question, QuestionCreateInput, QuestionUpdateInput } from '../../generated/prisma-client';
@@ -41,6 +42,36 @@ export const questions = {
   },
 
   async updateQuestion(parent, { input: { id, patch } }, ctx: Context, info) {
+    console.log('Patch imageUrl:');
+    console.log(patch.imageUrl);
+    // Check if imageUrl is different from the one in the database
+    // That would mean a new image was uploaded and needs to replace the old one.
+    const question = await ctx.db.question({ id });
+    console.log('Database imageUrl:');
+    console.log(question.imageUrl);
+    if (patch.imageUrl !== '' && patch.imageUrl !== question.imageUrl) {
+      const oldFile = path.join(ctx.appConfig.uploads.publicDir, question.imageUrl);
+      // Remove old image from the file system if it exists
+      fs.unlink(oldFile, err => {
+        if (err) {
+          throw err;
+        }
+        console.log('Old image existed, and it was deleted.');
+      });
+      // Move new image to public directory
+      const newFile = path.join(ctx.appConfig.uploads.tempDir, patch.imageUrl);
+      const destNewFile = path.join(ctx.appConfig.uploads.publicDir, patch.imageUrl);
+      // Maybe we don't need 'mv' package.
+      // https://nodejs.org/api/fs.html#fs_fs_rename_oldpath_newpath_callback
+      mv(newFile, destNewFile, err => {
+        if (err) {
+          console.error(
+            'There was an error moving the uploaded image from temp to public directory.',
+          );
+          throw new Error(err);
+        }
+      });
+    }
     const updatedQuestion = await ctx.db.updateQuestion({
       data: {
         ...patch,
