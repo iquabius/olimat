9241f87d243e9852c9fb3cc7cce4af650de54d24
Josias Iquabius <iquabius@gmail.com>
2018-11-18 22:19:46 -0300

    Set higher age and buffer length  for Next.js pages in dev

--- frontend/src/next.config.js
+++ frontend/src/next.config.js
@@ -20,4 +20,10 @@ module.exports = withCSS({
 
     return config;
   },
+  onDemandEntries: {
+    // Period (in ms) where the server will keep pages in the buffer
+    maxInactiveAge: 120 * 1e3, // default 25s
+    // Number of pages that should be kept simultaneously without being disposed
+    pagesBufferLength: 3, // default 2
+  },
 });
