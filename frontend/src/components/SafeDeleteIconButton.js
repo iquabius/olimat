8352c6762b3334abe56fcec0587ce3b9fa294067
Josias Iquabius <iquabius@gmail.com>
2018-11-27 11:43:20 -0300

    Add dialog to SafeDeleteIconButton in QuestionDetails

--- frontend/src/components/SafeDeleteIconButton.js
+++ frontend/src/components/SafeDeleteIconButton.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles, IconButton } from '@material-ui/core';
+import {
+  withStyles,
+  IconButton,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  Button,
+} from '@material-ui/core';
+import take from 'lodash/take';
 import DeleteIcon from '@material-ui/icons/Delete';
+import compose from 'recompose/compose';
+import { withState } from 'recompose';
 import DeleteConnector from './Question/DeleteConnector';
 import Router from 'next/router';
 
@@ -11,9 +23,13 @@ const styles = theme => ({
       color: 'inherit',
     },
   },
+  questionWording: {
+    fontStyle: 'italic',
+  },
 });
 
-const deleteSubmitHandler = (deleteQuestion, question) => () => {
+const deleteSubmitHandler = (deleteQuestion, question, setSubmitting) => () => {
+  setSubmitting(true);
   deleteQuestion({
     variables: {
       id: question.id,
@@ -22,6 +38,7 @@ const deleteSubmitHandler = (deleteQuestion, question) => () => {
     .then(response => {
       console.log(`Delete Question Mutation response: `); // eslint-disable-line no-console
       console.log(response); // eslint-disable-line no-console
+      setSubmitting(false);
       Router.push('/admin/questoes');
     })
     .catch(error => {
@@ -31,19 +48,78 @@ const deleteSubmitHandler = (deleteQuestion, question) => () => {
     });
 };
 
-// TODO: Show a warning dialog for confirmation before deleting
-function SafeDeleteIconButton({ classes, question, ...otherProps }) {
+const openDeleteWarningDialog = setDeleteWarningOpen => () => setDeleteWarningOpen(true);
+
+const onCancelDelete = (setDeleteWarningOpen, setSubmitting) => () => {
+  setSubmitting(false);
+  setDeleteWarningOpen(false);
+};
+
+/**
+ * @param {string} str String to truncate
+ */
+const truncate = str =>
+  take(str.split(' '), 7)
+    .join(' ')
+    .concat(' [...]');
+
+function SafeDeleteIconButton({
+  classes,
+  deleteWarningOpen,
+  question,
+  setDeleteWarningOpen,
+  setSubmitting,
+  submitting,
+  ...otherProps
+}) {
   return (
     <DeleteConnector>
       {deleteQuestion => (
         <React.Fragment>
           <IconButton
             className={classes.root}
-            onClick={deleteSubmitHandler(deleteQuestion, question)}
+            disabled={submitting}
+            onClick={openDeleteWarningDialog(setDeleteWarningOpen)}
             {...otherProps}
           >
             <DeleteIcon />
           </IconButton>
+          <Dialog
+            open={deleteWarningOpen}
+            onClose={onCancelDelete(setDeleteWarningOpen, setSubmitting)}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogTitle id="alert-dialog-title">
+              {'Excluir '}
+              <span className={classes.questionWording}>{truncate(question.wording)}</span>
+              {' ?'}
+            </DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                {'A questão '}
+                <span className={classes.questionWording}>{truncate(question.wording)}</span>
+                {' será apagada permanentemente.'}
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button
+                disabled={submitting}
+                onClick={onCancelDelete(setDeleteWarningOpen, setSubmitting)}
+                color="secondary"
+              >
+                Cancelar
+              </Button>
+              <Button
+                disabled={submitting}
+                onClick={deleteSubmitHandler(deleteQuestion, question, setSubmitting)}
+                color="secondary"
+                autoFocus
+              >
+                Excluir
+              </Button>
+            </DialogActions>
+          </Dialog>
         </React.Fragment>
       )}
     </DeleteConnector>
@@ -52,9 +128,17 @@ function SafeDeleteIconButton({ classes, question, ...otherProps }) {
 
 SafeDeleteIconButton.propTypes = {
   classes: PropTypes.object.isRequired,
+  deleteWarningOpen: PropTypes.bool.isRequired,
   question: PropTypes.shape({
     id: PropTypes.string.isRequired,
   }).isRequired,
+  setDeleteWarningOpen: PropTypes.func.isRequired,
+  setSubmitting: PropTypes.func.isRequired,
+  submitting: PropTypes.bool.isRequired,
 };
 
-export default withStyles(styles)(SafeDeleteIconButton);
+export default compose(
+  withState('deleteWarningOpen', 'setDeleteWarningOpen', false),
+  withState('submitting', 'setSubmitting', false),
+  withStyles(styles),
+)(SafeDeleteIconButton);
