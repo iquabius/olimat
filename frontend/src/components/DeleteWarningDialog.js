7ea7d1271ab2aa2d70aa84aab2feda560633d7ef
Josias Iquabius <iquabius@gmail.com>
2018-11-28 22:08:46 -0300

    Extract DeleteWarningDialog from SafeDeleteIconButton

---
+++ frontend/src/components/DeleteWarningDialog.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from '@material-ui/core';
+
+const DeleteWarningDialog = ({ content, isSubmitting, open, onCancel, onSuccess, title }) => (
+  <Dialog
+    open={open}
+    onClose={onCancel}
+    aria-labelledby="alert-dialog-title"
+    aria-describedby="alert-dialog-description"
+  >
+    <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
+    <DialogContent>
+      <DialogContentText id="alert-dialog-description">{content}</DialogContentText>
+    </DialogContent>
+    <DialogActions>
+      <Button disabled={isSubmitting} onClick={onCancel} color="secondary">
+        Cancelar
+      </Button>
+      <Button disabled={isSubmitting} onClick={onSuccess} color="secondary" autoFocus>
+        Excluir
+      </Button>
+    </DialogActions>
+  </Dialog>
+);
+
+DeleteWarningDialog.propTypes = {
+  content: PropTypes.string.isRequired,
+  isSubmitting: PropTypes.bool.isRequired,
+  onCancel: PropTypes.func.isRequired,
+  onSuccess: PropTypes.func.isRequired,
+  open: PropTypes.bool,
+  title: PropTypes.string.isRequired,
+};
+
+export default DeleteWarningDialog;
