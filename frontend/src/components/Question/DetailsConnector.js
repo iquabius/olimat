1b25b9cf891b75c4de51b6350920a71495b5cea3
Josias Iquabius <iquabius@gmail.com>
2018-12-03 05:50:14 -0300

    Remove cache update from QuestionDetailsConnector
    Apollo updates automatically, because questionQuery
    is fetched by an unique id.

--- frontend/src/components/Question/DetailsConnector.js
+++ frontend/src/components/Question/DetailsConnector.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import gql from 'graphql-tag';
 import { Query, Mutation } from 'react-apollo';
-import { questionsConnectionQuery } from './ListConnector';
 
 export const questionQuery = gql`
   query questionQuery($id: ID!) {
@@ -37,24 +36,9 @@ export const updateQuestionMutation = gql`
   }
 `;
 
-// Atualiza o cache do Apollo com as alterações realizadas
-const updateApolloStore = (proxy, { data: { updateQuestion } }) => {
-  try {
-    const cacheData = proxy.readQuery({ query: questionsConnectionQuery });
-    const questionEdge = {
-      cursor: updateQuestion.question.id,
-      node: updateQuestion.question,
-    };
-    cacheData.questionsConnection.unshift(questionEdge);
-
-    proxy.writeQuery({ query: questionsConnectionQuery, cacheData });
-  } catch (cacheError) {
-    // Do nothing. Questions were not fetched yet.
-  }
-};
-
 // Maybe put this in responseToFormValues()
 const filesHost = 'http://localhost:4000/files';
+
 export const questionWithFullUrl = question => ({
   ...question,
   imageUrl: question.imageUrl,
@@ -65,7 +49,7 @@ export const questionWithFullUrl = question => ({
 const QuestionDetailsConnector = ({ children, id }) => (
   <Query query={questionQuery} variables={{ id }}>
     {({ data, error, loading }) => (
-      <Mutation mutation={updateQuestionMutation} update={updateApolloStore}>
+      <Mutation mutation={updateQuestionMutation}>
         {updateQuestion => {
           if (loading) return <h1>Carregando questão...</h1>;
           // TODO: Handle error when the backend is down
