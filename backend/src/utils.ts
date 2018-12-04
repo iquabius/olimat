9e49616db9b0814376c10bd9e8808857bb71b913
Josias Iquabius <iquabius@gmail.com>
2018-12-04 03:11:29 -0300

    Fix import statements in backend/src/utils.ts

--- backend/src/utils.ts
+++ backend/src/utils.ts
@@ -1,5 +1,5 @@
-import * as express from 'express';
-import * as jwt from 'jsonwebtoken';
+import express from 'express';
+import jwt from 'jsonwebtoken';
 import { Prisma } from './generated/prisma-client';
 import { Prisma as PrismaBinding } from 'prisma-binding';
 
