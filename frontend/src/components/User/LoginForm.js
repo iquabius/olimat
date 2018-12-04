dfee1a1c4b088b06314c8e47b0a23d295a12739f
Josias Iquabius <iquabius@gmail.com>
2018-12-04 04:41:34 -0300

    Refactor LoginForm to use LoginConnector

--- frontend/src/components/User/LoginForm.js
+++ frontend/src/components/User/LoginForm.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { graphql, withApollo, compose } from 'react-apollo';
-import cookie from 'cookie';
-import gql from 'graphql-tag';
+import { compose } from 'react-apollo';
 import { withStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
 import {
@@ -21,7 +19,8 @@ import Button from '@material-ui/core/Button';
 import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
 import Link from '../Link';
-import redirect from '../../utils/redirect';
+import LoginConnector from './LoginConnector';
+import { fromRenderProps } from 'recompose';
 
 const styles = theme => ({
   loginBox: {
@@ -71,7 +70,7 @@ class LoginForm extends React.Component {
   };
 
   render() {
-    const { classes } = this.props;
+    const { classes, handleSignIn } = this.props;
     const showPasswordAdornment = (
       <InputAdornment position="end">
         <IconButton
@@ -90,7 +89,7 @@ class LoginForm extends React.Component {
           Acesse sua Conta
         </Typography>
         <Divider />
-        <form onSubmit={this.props.handleSignIn}>
+        <form onSubmit={handleSignIn}>
           <TextField
             name="email"
             label="Email"
@@ -147,63 +146,9 @@ class LoginForm extends React.Component {
   }
 }
 
+const loginConnector = fromRenderProps(LoginConnector, ({ handleSignIn }) => ({ handleSignIn }));
+
 export default compose(
+  loginConnector,
   withStyles(styles),
-  // withApollo exposes `this.props.client` used when logging out
-  withApollo,
-  graphql(
-    // The `login` mutation is provided by the backend
-    gql`
-      mutation Signin($email: String!, $password: String!) {
-        login(email: $email, password: $password) {
-          token
-        }
-      }
-    `,
-    {
-      // Use an unambiguous name for use in the `props` section below
-      name: 'signinWithEmail',
-      // Apollo's way of injecting new props which are passed to the component
-      props: ({
-        signinWithEmail,
-        // `client` is provided by the `withApollo` HOC
-        ownProps: { client },
-      }) => ({
-        // `signin` is the name of the prop passed to the component
-        handleSignIn: event => {
-          /* global FormData */
-          const data = new FormData(event.target);
-          console.log(data);
-
-          event.preventDefault();
-          event.stopPropagation();
-
-          signinWithEmail({
-            variables: {
-              email: data.get('email'),
-              password: data.get('password'),
-            },
-          })
-            .then(({ data: { login: { token } } }) => {
-              // Store the token in cookie
-              document.cookie = cookie.serialize('token', token, {
-                maxAge: 30 * 24 * 60 * 60, // 30 days
-              });
-
-              // Force a reload of all the current queries now that the user is
-              // logged in
-              client.resetStore().then(() => {
-                // Now redirect to the homepage
-                redirect({}, '/');
-              });
-            })
-            .catch(error => {
-              // Something went wrong, such as incorrect password, or no network
-              // available, etc.
-              console.error(error);
-            });
-        },
-      }),
-    },
-  ),
 )(LoginForm);
