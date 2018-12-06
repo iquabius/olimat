442f473a3db3582136195068ed2310f1a71df7bd
Josias Iquabius <iquabius@gmail.com>
2018-12-06 03:08:06 -0300

    Add page /admin/provas and drawer link

---
+++ frontend/src/pages/admin/provas/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AppFrame from '../../../components/AppFrame';
+import AppContent from '../../../components/AppContent';
+import TestList from '../../../components/Test/List';
+
+const PageAdminTests = () => (
+  <AppFrame>
+    <AppContent>
+      <TestList />
+    </AppContent>
+  </AppFrame>
+);
+
+export default PageAdminTests;
