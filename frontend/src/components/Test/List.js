efa3f898e1c77b9f02a4a6b228f513ae244d8fd7
Josias Iquabius <iquabius@gmail.com>
2018-12-06 02:41:41 -0300

    Use Material-UI's List in TestList

--- frontend/src/components/Test/List.js
+++ frontend/src/components/Test/List.js
@@ -1,25 +1,55 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import ListConnector from './ListConnector';
+import {
+  List,
+  withStyles,
+  ListItem,
+  ListItemText,
+  ListItemSecondaryAction,
+  IconButton,
+} from '@material-ui/core';
+import EditIcon from '@material-ui/icons/Edit';
+// eslint-disable-next-line no-unused-vars
+import { Theme } from '@material-ui/core/styles/createMuiTheme';
+// eslint-disable-next-line no-unused-vars
+import { StyleRules } from '@material-ui/core/styles/withStyles';
 
-function TestList() {
+/**
+ * This callback function is used to create the styles
+ * @param {Theme} theme Material-UI theme
+ * @return {StyleRules}
+ */
+const styles = theme => ({
+  root: {
+    ...theme.shape,
+    backgroundColor: theme.palette.background.paper,
+  },
+});
+
+function TestList({ classes }) {
   return (
     <ListConnector>
       {({ tests }) => (
-        <section>
-          <ul>
-            {tests.map((test, index) => (
-              <li key={test.id}>
-                <div>
-                  <span>{`${index + 1}. `}</span>
-                  {test.title}
-                </div>
-              </li>
-            ))}
-          </ul>
-        </section>
+        <List className={classes.root}>
+          {tests.map(test => (
+            <ListItem key={test.id} role={undefined} dense button onClick={() => {}}>
+              <ListItemText primary={`Line item ${test.title + 1}`} />
+              <ListItemSecondaryAction>
+                <IconButton aria-label="Editar prova">
+                  <EditIcon />
+                </IconButton>
+              </ListItemSecondaryAction>
+            </ListItem>
+          ))}
+        </List>
       )}
     </ListConnector>
   );
 }
 
-export default TestList;
+TestList.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(TestList);
