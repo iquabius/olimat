c3ebef6e9859e997eac80c7091df3d229c7e168c
Josias Iquabius <iquabius@gmail.com>
2018-12-10 22:43:11 -0300

    Add first test for pageToTitle helper function

---
+++ frontend/src/utils/helpers.test.js
@@ -0,0 +1,11 @@
+/* eslint-env jest */
+import { pageToTitle } from './helpers';
+
+describe('pageToTitle', () => {
+  test('returns own title property if it exists', () => {
+    const page = {
+      title: 'OliMAT',
+    };
+    expect(pageToTitle(page)).toBe('OliMAT');
+  });
+});
