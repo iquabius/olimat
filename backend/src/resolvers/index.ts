22a5ad6aac5212767ec7ae1f6a6099e2cc0222cf
Josias Iquabius <iquabius@gmail.com>
2019-01-01 20:24:13 -0300

    [back] Create index.ts for Mutations

--- backend/src/resolvers/index.ts
+++ backend/src/resolvers/index.ts
@@ -1,10 +1,5 @@
 import { Query } from './Query';
-import { auth } from './Mutation/auth';
-import { cities } from './Mutation/cities';
-import { tests } from './Mutation/tests';
-import { schools } from './Mutation/schools';
-import { olympiads } from './Mutation/olympiads';
-import { questions } from './Mutation/questions';
+import { Mutation } from './Mutation';
 import { AuthPayload } from './AuthPayload';
 import { Node } from './Node';
 import { Question } from './Question';
@@ -15,13 +10,6 @@ export default {
   Test,
   Question,
   Node,
-  Mutation: {
-    ...cities,
-    ...auth,
-    ...tests,
-    ...schools,
-    ...olympiads,
-    ...questions,
-  },
+  Mutation,
   AuthPayload,
 };
