5b1932541b550a305c4e26403dc4b47d4bec5c13
Josias Iquabius <iquabius@gmail.com>
2019-01-02 20:07:54 -0300

    [back] Change AuthPayload.user function's format

--- backend/src/resolvers/AuthPayload.ts
+++ backend/src/resolvers/AuthPayload.ts
@@ -1,7 +1,7 @@
 import { AuthPayloadResolvers } from '../__generated__/resolvers-types';
 
 export const AuthPayload: AuthPayloadResolvers.Resolvers = {
-  user: async ({ user: { id } }, args, ctx, info) => {
+  user({ user: { id } }, args, ctx, info) {
     return ctx.prisma.user({ id });
   },
 };
