3ab078ce5cdc72c2349e99e40948ff0fe28de178
Josias Iquabius <iquabius@gmail.com>
2019-01-08 05:20:24 -0300

    Add QuestionDetails test for error on deletion

--- frontend/src/components/Question/Details.test.js
+++ frontend/src/components/Question/Details.test.js
@@ -8,6 +8,9 @@ import { SnackbarProvider } from 'notistack';
 import FakeDataProvider from '../../utils/test/FakeDataProvider';
 import MockErrorProvider from '../../utils/test/MockErrorProvider';
 import MockNextContext from '../../utils/test/MockNextContext';
+import mockGraphql from '../../utils/test/mockGraphql';
+import { questionQuery } from './DetailsConnector';
+import { deleteQuestionMutation } from './DeleteConnector';
 
 const MockQuestionDetails = () => (
   <MockNextContext router={{ query: { id: 'theQuestionId1' } }}>
@@ -111,6 +114,58 @@ describe('<QuestionDetails />', () => {
     expect(confirmDeleteButton).toBeDisabled();
     await waitForElement(() => getByText('Questão excluída'));
     expect(Router.pathname).toMatchSnapshot();
+  });
+
+  // We can't use <FakeDataProvider/> because there's no way to mock an error.
+  test('clicking on confirm button shows error if delete fails', async () => {
+    const data = await mockGraphql(questionQuery, { id: '' });
+    const errorMsg = 'Error deleting the question, try again.';
+
+    const mocks = [
+      {
+        request: { query: questionQuery, variables: { id: data.question.id } },
+        result: { data },
+      },
+      // Network error
+      // {
+      //   request: { query: deleteQuestionMutation, variables: { id: data.question.id } },
+      //   error: new Error(errorMsg),
+      // },
+      {
+        request: { query: deleteQuestionMutation, variables: { id: data.question.id } },
+        result: { errors: [{ message: errorMsg }] },
+      },
+    ];
+
+    const { getByLabelText, getByText } = renderApollo(
+      <MockNextContext router={{ query: { id: data.question.id } }}>
+        <SnackbarProvider maxSnack={3}>
+          <QuestionDetails />
+        </SnackbarProvider>
+      </MockNextContext>,
+      { mocks },
+    );
+
+    // Espera a questão ser carregada
+    await waitForElement(() => getByLabelText('Excluir questão'));
+
+    // Clica no botão pra excluir, mostra aviso
+    const deleteButton = getByLabelText('Excluir questão');
+    fireEvent.click(deleteButton);
+    // Então clica no botão de confirmar (dispara a 'Mutation')
+    const confirmDeleteButton = getByLabelText('Confirmar excluir questão');
+    fireEvent.click(confirmDeleteButton);
+
+    // Confere se os botões estão desativados enquanto a mutation é processada
+    expect(deleteButton).toBeDisabled();
+    expect(confirmDeleteButton).toBeDisabled();
+    // Espera o erro aparecer
+    await waitForElement(() => getByText(/Erro ao excluir questão:/));
+    // Confere se o erro inclui a mensagem passada pelo Apollo Client
+    expect(getByText(/Erro ao excluir questão:/)).toHaveTextContent(errorMsg);
+    // Confere se o botão está ativo depois do erro
+    expect(confirmDeleteButton).not.toBeDisabled();
+    expect(deleteButton).toBeDisabled();
   });
 
   test('clicking edit link should go to edit form page', async () => {
