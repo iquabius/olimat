7ac784c6a7da6a3ba9c6506eece9b1b5fa667828
Josias Iquabius <iquabius@gmail.com>
2019-01-08 15:59:39 -0300

    Add FakeLoadingProvider for testing loading state
    It's commented because we're using renderApollo() for testing loading
    state. It could be useful if we start using Styleguidist or Storybooks.

---
+++ frontend/src/utils/test/FakeLoadingProvider.js
@@ -0,0 +1,21 @@
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import { ApolloLink, Observable } from 'apollo-link';
+// import ApolloClient from 'apollo-client';
+// import { InMemoryCache } from 'apollo-cache-inmemory';
+// import { ApolloProvider } from 'react-apollo';
+
+// const FakeLoadingProvider = ({ children }) => {
+//   const client = new ApolloClient({
+//     link: new ApolloLink(() => new Observable(() => {})),
+//     cache: new InMemoryCache(),
+//   });
+
+//   return <ApolloProvider client={client}>{children}</ApolloProvider>;
+// };
+
+// FakeLoadingProvider.propTypes = {
+//   children: PropTypes.node.isRequired,
+// };
+
+// export default FakeLoadingProvider;
