72de4febf3a69a31d7e75e832ce74574ac9b4589
Josias Iquabius <iquabius@gmail.com>
2019-01-20 02:49:22 -0300

    Add type for global.__INSERTION_POINT__

--- web/global.d.ts
+++ web/global.d.ts
@@ -1,6 +1,7 @@
 declare namespace NodeJS {
   // interface Global extends NodeJS.Global {}
   interface Global extends GlobalFetch {
+    __INSERTION_POINT__: boolean;
     // We can't import PageContext here, what to do?
     // https://stackoverflow.com/a/45422775/1787829
     __MUI_PAGE_CONTEXT__: object;
