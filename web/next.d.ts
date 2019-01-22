75fae2442ae3122b3b59f2dd85020e8265bb24fa
Josias Iquabius <iquabius@gmail.com>
2019-01-22 02:50:56 -0300

    Extend NextContext interface to include apolloClient

---
+++ web/next.d.ts
@@ -0,0 +1,9 @@
+import ApolloClient from 'apollo-client';
+import { NextContext as DefaultNextContext } from 'next';
+
+// Extends default NextContext to include apolloClient
+declare module 'next' {
+  interface NextContext extends DefaultNextContext {
+    apolloClient: ApolloClient<any>;
+  }
+}
