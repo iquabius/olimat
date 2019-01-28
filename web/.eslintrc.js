bb2dff72966e05401d6d156a01f397314a6ffee0
Josias Iquabius <iquabius@gmail.com>
2019-01-27 23:21:15 -0300

    Remove parseOptions.ecmaFeatures.jsx in ESLint conf
    We don't need this since we're extending the recommend config:
    https://github.com/yannickcr/eslint-plugin-react/blob/master/index.js#L122

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -2,11 +2,6 @@ const React = require('react');
 
 module.exports = {
   extends: 'plugin:react/recommended',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
   rules: {
     // We're using TypeScript now, no need for PropTypes everywhere
     'react/prop-types': ['error', { skipUndeclared: true }],
