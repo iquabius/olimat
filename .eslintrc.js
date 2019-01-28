8d4dd851f681ffa0b5133081323379ae8c422bdc
Josias Iquabius <iquabius@gmail.com>
2019-01-28 04:29:22 -0300

    Set up eslint-plugin-import

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,14 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
+  plugins: ['import'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     // Disable all rules related to code formatting. Prettier handles formatting.
     // Prettier related configs should come last in the 'extends' Array.
+    'plugin:import/recommended',
+    // We could move the import/* settings below to a lerna package, and use it here:
+    // 'plugin:typescript-eslint-import/recommended',
     'plugin:prettier/recommended',
     'prettier/@typescript-eslint',
   ],
@@ -14,5 +18,20 @@ module.exports = {
     '@typescript-eslint/explicit-member-accessibility': 'off',
     '@typescript-eslint/no-empty-interface': 'warn',
     '@typescript-eslint/no-use-before-define': 'off',
+
+    // eslint-plugin-import
+    // We can't alphabetize imports within groups, yet
+    // https://github.com/benmosher/eslint-plugin-import/pull/1105
+    'import/order': ['error'],
+  },
+  settings: {
+    'import/extensions': ['.ts', '.tsx'],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    // https://www.npmjs.com/package/eslint-import-resolver-typescript#configuration
+    'import/resolver': {
+      typescript: {},
+    },
   },
 };
