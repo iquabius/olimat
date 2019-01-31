925ac37d02425dff99c28d4af05b9e7eccbf823e
Josias Iquabius <iquabius@gmail.com>
2019-01-31 08:14:32 -0300

    Ignore '@olimat/api' globally for import/no-unresolved
    For some reason eslint can't resolve @olimat/api used in @olimat/web.
    It's symlinked by lerna, so it should just work. It's not the best
    solution but we're ignoring globally for now.

--- .eslintrc.js
+++ .eslintrc.js
@@ -40,6 +40,10 @@ module.exports = {
         ],
       },
     ],
+    // It seems eslint-import-resolver-typescript isn't properly handling the
+    // 'paths' defined in packages/api/tsconfig.json, so we need to ignore any
+    // custom path we define there.
+    'import/no-unresolved': ['error', { ignore: ['^@olimat/api']}],
     'import/order': ['error'],
   },
   settings: {
