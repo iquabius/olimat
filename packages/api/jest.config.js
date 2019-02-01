cd55271c9d40eccf33e1c61f0ae0de8f1cb8ecc6
Josias Iquabius <iquabius@gmail.com>
2019-02-01 17:08:41 -0300

    Fix 'test:watch' npm script
    I'm not sure why 'test:watch' is failing but 'test' isn't.

--- packages/api/jest.config.js
+++ packages/api/jest.config.js
@@ -4,6 +4,11 @@ const baseConfig = require('../../jest.workspace');
 module.exports = {
   ...baseConfig,
   displayName: 'API',
+  // ts-jest has a helper that can be used for this:
+  // https://kulshekhar.github.io/ts-jest/user/config/#paths-mapping
+  moduleNameMapper: {
+    '\\@olimat/api/(.*)': '<rootDir>/dist/$1',
+  },
   setupFiles: ['<rootDir>/jest.setup.ts'],
   transform: {
     '\\.ts$': 'ts-jest',
