dc4d55e81d3e5e2a08d21a85165ff2ab14a25285
Josias Iquabius <iquabius@gmail.com>
2019-02-01 19:54:37 -0300

    Refactor config and npm scripts (#144)

--- packages/api/jest.config.js
+++ packages/api/jest.config.js
@@ -1,12 +1,13 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const baseConfig = require('../../jest.workspace');
-
 module.exports = {
-  ...baseConfig,
   displayName: 'API',
+  // ts-jest has a helper that can be used for this:
+  // https://kulshekhar.github.io/ts-jest/user/config/#paths-mapping
+  moduleNameMapper: {
+    '\\@olimat/api/(.*)': '<rootDir>/dist/$1',
+  },
   setupFiles: ['<rootDir>/jest.setup.ts'],
   transform: {
-    '\\.ts?$': 'ts-jest',
+    '\\.ts$': 'ts-jest',
   },
   testPathIgnorePatterns: ['dist', '/node_modules/', '/__utils'],
 };
