760098455393eacca5bb37a83b770813cc1df21e
Josias Iquabius <iquabius@gmail.com>
2020-02-28 00:32:30 -0400

    Extrai `pages` de _app.tsx p/ src/pages.ts

--- packages/web/src/pages/_app.tsx
+++ packages/web/src/pages/_app.tsx
@@ -9,113 +9,11 @@ import { SnackbarProvider } from 'notistack';
 import React from 'react';
 
 // import AppWrapper from '../components/AppWrapper';
-import PageContext, { LoggedInUser, Page } from '../components/PageContext';
+import PageContext, { LoggedInUser } from '../components/PageContext';
 import checkLoggedIn from '../utils/checkLoggedIn';
 import withData from '../utils/withData';
 import { ThemeProvider } from '../components/ThemeContext';
-
-const pages: Page[] = [
-  {
-    pathname: '/material-didatico',
-    children: [
-      {
-        pathname: '/material-didatico/provas',
-        title: 'Provas e Soluções',
-      },
-      {
-        pathname: '/material-didatico/questoes',
-        title: 'Banco de Questões',
-      },
-      {
-        pathname: '/material-didatico/simulados',
-      },
-    ],
-  },
-  {
-    pathname: '/programas-e-portais',
-    children: [
-      {
-        pathname: '/programas/poti',
-        title: 'POTI',
-      },
-      {
-        pathname: '/programas/portal-da-matematica',
-        title: 'Portal da Matemática',
-      },
-    ],
-  },
-  {
-    pathname: '/edicoes-anteriores',
-    children: [
-      {
-        pathname: '/edicoes-anteriores/2017',
-      },
-      {
-        pathname: '/edicoes-anteriores/2016',
-      },
-      {
-        pathname: '/edicoes-anteriores/2015',
-      },
-      {
-        pathname: '/edicoes-anteriores/2014',
-      },
-      {
-        pathname: '/edicoes-anteriores/mais-antigas',
-      },
-    ],
-  },
-  {
-    pathname: '/',
-    displayNav: false,
-    title: false,
-  },
-  {
-    pathname: '/admin',
-    title: 'Painel de Administração',
-    children: [
-      {
-        pathname: '/admin/questao-criar',
-        title: 'Criar Questão',
-        displayNav: false,
-      },
-      {
-        pathname: '/admin/questao-editar',
-        title: 'Editar Questão',
-        displayNav: false,
-      },
-      {
-        pathname: '/admin/questao',
-        title: 'Detalhes da Questão',
-        displayNav: false,
-      },
-      {
-        pathname: '/admin/questoes',
-        title: 'Questões',
-      },
-      {
-        pathname: '/admin/provas',
-        title: 'Provas',
-      },
-      {
-        pathname: '/admin/provas/detalhes',
-        title: 'Detalhes da Prova',
-        displayNav: false,
-      },
-      {
-        pathname: '/admin/cidades',
-        title: 'Cidades',
-      },
-      {
-        pathname: '/admin/escolas',
-        title: 'Escolas',
-      },
-      {
-        pathname: '/admin/olimpiadas',
-        title: 'Olimpíadas',
-      },
-    ],
-  },
-];
+import { pages } from '../pages';
 
 function findActivePage(currentPages, pathname) {
   const activePage = find(currentPages, page => {
