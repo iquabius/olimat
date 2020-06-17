dac8cd94aad518bbfca3d805b4cbb344a45812b9
Josias Iquabius <iquabius@gmail.com>
2020-06-17 15:55:03 -0400

    Suppress TS errors from mocked server context

--- packages/api/src/__tests__/resolvers/__utils/mockContext.ts
+++ packages/api/src/__tests__/resolvers/__utils/mockContext.ts
@@ -5,6 +5,8 @@ import { OliContext } from '@olimat/api/utils';
 const createMockContext = jest.fn<OliContext, []>(() => ({
 	// @ts-ignore
 	prisma: {
+		// This gives an error when using it:
+		// "error TS2339: Property 'mockReturnValueOnce' does not exist on type"
 		question: jest.fn(),
 		exam: jest.fn(),
 	},
