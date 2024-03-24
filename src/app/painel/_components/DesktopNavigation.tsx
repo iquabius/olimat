da98fc29111089439714958ee0ac5ebfbe88d8d6
Josias Iquabius <iquabius@gmail.com>
2024-03-23 23:30:04 -0400

    Extract DesktopNavigation

---
+++ src/app/painel/_components/DesktopNavigation.tsx
@@ -0,0 +1,52 @@
+import Link from "next/link";
+import { Package2 } from "lucide-react";
+
+export function DesktopNavigation() {
+	return (
+		<nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+			<Link
+				href="#"
+				className="flex items-center gap-2 text-lg font-semibold md:text-base"
+			>
+				<Package2 className="h-6 w-6" />
+				<span className="sr-only">Acme Inc</span>
+			</Link>
+			<Link
+				href="#"
+				className="text-foreground transition-colors hover:text-foreground"
+			>
+				Painel
+			</Link>
+			<Link
+				href="#"
+				className="text-muted-foreground transition-colors hover:text-foreground"
+			>
+				Escolas
+			</Link>
+			<Link
+				href="#"
+				className="text-muted-foreground transition-colors hover:text-foreground"
+			>
+				Cidades
+			</Link>
+			<Link
+				href="#"
+				className="text-muted-foreground transition-colors hover:text-foreground"
+			>
+				Provas
+			</Link>
+			<Link
+				href="#"
+				className="text-muted-foreground transition-colors hover:text-foreground"
+			>
+				Questões
+			</Link>
+			<Link
+				href="#"
+				className="text-muted-foreground transition-colors hover:text-foreground"
+			>
+				Olimpíadas
+			</Link>
+		</nav>
+	);
+}
--- src/app/painel/page.tsx
+++ src/app/painel/page.tsx
@@ -38,56 +38,13 @@ import {
 	TableHeader,
 	TableRow,
 } from "~/components/ui/table";
+import { DesktopNavigation } from "./_components/DesktopNavigation";
 
 export default function Dashboard() {
 	return (
 		<div className="flex min-h-screen w-full flex-col">
 			<header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
-				<nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
-					<Link
-						href="#"
-						className="flex items-center gap-2 text-lg font-semibold md:text-base"
-					>
-						<Package2 className="h-6 w-6" />
-						<span className="sr-only">Acme Inc</span>
-					</Link>
-					<Link
-						href="#"
-						className="text-foreground transition-colors hover:text-foreground"
-					>
-						Painel
-					</Link>
-					<Link
-						href="#"
-						className="text-muted-foreground transition-colors hover:text-foreground"
-					>
-						Escolas
-					</Link>
-					<Link
-						href="#"
-						className="text-muted-foreground transition-colors hover:text-foreground"
-					>
-						Cidades
-					</Link>
-					<Link
-						href="#"
-						className="text-muted-foreground transition-colors hover:text-foreground"
-					>
-						Provas
-					</Link>
-					<Link
-						href="#"
-						className="text-muted-foreground transition-colors hover:text-foreground"
-					>
-						Questões
-					</Link>
-					<Link
-						href="#"
-						className="text-muted-foreground transition-colors hover:text-foreground"
-					>
-						Olimpíadas
-					</Link>
-				</nav>
+				<DesktopNavigation />
 				<Sheet>
 					<SheetTrigger asChild>
 						<Button
