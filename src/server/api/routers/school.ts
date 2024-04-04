e2859d0c74069c77491133a57e0f6d9b573b8dc6
Josias Iquabius <iquabius@gmail.com>
2024-04-04 06:14:44 -0400

    Remove unused `ctx` from School.create route

--- src/server/api/routers/school.ts
+++ src/server/api/routers/school.ts
@@ -2,11 +2,9 @@ import { SchoolAddSchema } from "~/shared/schemas";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 
 export const schoolRouter = createTRPCRouter({
-	create: protectedProcedure
-		.input(SchoolAddSchema)
-		.mutation(({ ctx, input }) => {
-			// TODO: Set up School table
-			console.log({ input });
-			return input;
-		}),
+	create: protectedProcedure.input(SchoolAddSchema).mutation(({ input }) => {
+		// TODO: Set up School table
+		console.log({ input });
+		return input;
+	}),
 });
