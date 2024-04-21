d31739327bb0fcfbe53a9a4c35297de617680910
Josias Iquabius <iquabius@gmail.com>
2024-04-21 13:50:58 -0400

    Move mock schools to tRPC endpoint

--- src/app/painel/escolas/page.tsx
+++ src/app/painel/escolas/page.tsx
@@ -1,41 +1,9 @@
 import { DataTable } from "./_components/data-table/data-table";
-import { columns, type School } from "./_components/data-table/columns";
+import { columns } from "./_components/data-table/columns";
+import { api } from "~/trpc/server";
 
 export default async function DashboardSchoolsPage() {
-	const data = await getData();
+	const data = await api.school.findMany();
 
 	return <DataTable columns={columns} data={data} />;
 }
-
-const getData: () => Promise<School[]> = async () => {
-	return [
-		{
-			id: "NytEqLA7nr",
-			name: "Guiomar de Campos Miranda",
-			city: "Barra do Bugres",
-			studentsQuantity: 101,
-			phoneNumber: "(68) 5778-0487",
-		},
-		{
-			id: "lJcBxwkY2N",
-			name: "Alfredo José da Silva",
-			city: "Barra do Bugres",
-			studentsQuantity: 99,
-			phoneNumber: "(47) 28806-8671",
-		},
-		{
-			id: "aUu7wby9ei",
-			name: "Ee Indígena Jula Pare",
-			city: "Barra do Bugres",
-			studentsQuantity: 71,
-			phoneNumber: "(85) 1072-0179",
-		},
-		{
-			id: "dskgLKKIVb",
-			name: "Herculano Borges",
-			city: "Barra do Bugres",
-			studentsQuantity: 69,
-			phoneNumber: "(83) 7370-2389",
-		},
-	];
-};
--- src/server/api/routers/school.ts
+++ src/server/api/routers/school.ts
@@ -1,10 +1,42 @@
 import { SchoolAddSchema } from "~/shared/schemas";
-import { createTRPCRouter, protectedProcedure } from "../trpc";
+import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
 
 export const schoolRouter = createTRPCRouter({
 	create: protectedProcedure.input(SchoolAddSchema).mutation(({ input }) => {
 		// TODO: Set up School table
 		console.log({ input });
 		return input;
+	}),
+	findMany: publicProcedure.query(() => {
+		return [
+			{
+				id: "NytEqLA7nr",
+				name: "Guiomar de Campos Miranda",
+				city: "Barra do Bugres",
+				studentsQuantity: 101,
+				phoneNumber: "(68) 5778-0487",
+			},
+			{
+				id: "lJcBxwkY2N",
+				name: "Alfredo José da Silva",
+				city: "Barra do Bugres",
+				studentsQuantity: 99,
+				phoneNumber: "(47) 28806-8671",
+			},
+			{
+				id: "aUu7wby9ei",
+				name: "Ee Indígena Jula Pare",
+				city: "Barra do Bugres",
+				studentsQuantity: 71,
+				phoneNumber: "(85) 1072-0179",
+			},
+			{
+				id: "dskgLKKIVb",
+				name: "Herculano Borges",
+				city: "Barra do Bugres",
+				studentsQuantity: 69,
+				phoneNumber: "(83) 7370-2389",
+			},
+		];
 	}),
 });
